0b90acfba5b2e77ebbcf16195d9077582d538042
zaelani5501 <fajarhabibzaelani@gmail.com>
2024-02-27 16:27:07 +0700

    koversi number

--- src/TipeDataNumber.java
+++ src/TipeDataNumber.java
@@ -5,5 +5,16 @@ public class TipeDataNumber {
         int iniInt = 1000000;
         long iniLong = 1000000;
         long iniLong2 = 1000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFFFF;
+        int binaryDecimal = 0b10101010;
+
+        int amount = 1_000_000_000;
+
+        System.out.println( "Jumlah" + amount);
     }
 }
